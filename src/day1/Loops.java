c49fa633a68a51b0d765bbb1befa3ed36c85b20e
karinaqapro@gmail.com <karinaqapro@gmail.com>
2020-06-08 22:35:15 -0400

    adding loops to the git

---
+++ src/day1/Loops.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Loops {
+    public static void main(String []args){
+        String c = "loops";
+        System.out.println(c);
+        System.out.println(c);
+    }
+}
